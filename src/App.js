af953ea8d5e110b9967ff6e5b6b3c29aab412d9a
Scotterpop73 <sctzone@aol.com>
2022-02-10 09:26:22 -0800

    Influential Person Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+
+import AboutComponent from "./components/AboutComponent";
+import Title from "./components/Title";
 
 function App() {
+  //Variable and the function to set it
+  const [components, setComponents] = useState([
+    { Title: "About", Component: AboutComponent },
+    { Title: "Awards", Component: "" },
+    { Title: "Influence", Component: "" },
+    { Title: "", Component: Title },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App-header">
+        <Container fluid>
+          <Row className="p-0">
+            <Col className="p-0">
+              <Navbar bg="light" expand="lg">
+                <Container fluid>
+                  <Navbar.Brand href="/">Home</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse
+                    id="basic-navbar-nav"
+                    className="justify-content-end"
+                  >
+                    <Nav>
+                      {components.map((component, idx) => {
+                        let { Component, Title } = component;
+
+                        return (
+                          <Nav.Item key={idx}>
+                            <Nav.Link href={Title}>{Title}</Nav.Link>
+                          </Nav.Item>
+                        );
+                      })}
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
+            </Col>
+          </Row>
+          <Routes>
+            {components.map((Link, idx) => {
+              let { Component, Title } = Link;
+              return <Route path={Title} element={<Component />} key={idx} />;
+            })}
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Title.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Row, Col, Carousel } from "react-bootstrap";
+import DisneyStatue from "../Assets/DisneyStatue.jpg";
+
+export default function Title() {
+  return (
+    <div>
+      <Row className="mt-5">
+        <Col className="d-flex justify-content-center">
+          <h1>
+            <strong>Walt Disney</strong>
+          </h1>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+    </div>
+  );
+}
